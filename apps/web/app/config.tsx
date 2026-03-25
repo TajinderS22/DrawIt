34de47d1952bc7ba7952bb21e8238933daaa3104
TajinderS22 <singhtajinder9888@gmail.com>
2026-03-25 12:56:38 +0530

    updated BE URL

--- apps/web/app/config.tsx
+++ apps/web/app/config.tsx
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://apidrawit.tajinder.in";
-export const BACKEND_URL = "http://localhost:3003";
+export const BACKEND_URL = "https://apidrawit.tajinder.in";
+// export const BACKEND_URL = "http://localhost:3003";
 export const WEBSOCKET_URL = "wss://wsdrawit.tajinder.in";
